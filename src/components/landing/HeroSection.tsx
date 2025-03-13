bf5718ce7917d3b911c988ca67c6975f6d0f03eb
Ch1mpleo <vietanh051203@gmail.com>
2025-03-13 12:49:53 +0700

    minor change

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -2,8 +2,7 @@
 
 import { motion } from "framer-motion"
 import BlurText from "@/components/ui/blur-text"
-
-const heroImg = "https://minio.ae-tao-fullstack-api.site/api/v1/buckets/arwoh-bucket/objects/download?preview=true&prefix=landingPage%2Fhero.png&version_id=null"
+import HeroImg from "@/assets/images/heroImage.png"
 
 export default function HeroSection() {
 	return (
@@ -40,7 +39,7 @@ export default function HeroSection() {
 
 			{/* Background artwork - removed drop shadow */}
 			<div className="absolute right-0 top-1/2 -translate-y-1/2 w-1/2 h-[80vh]">
-				<img src={heroImg || "/placeholder.svg"} alt="3D Artwork" className="w-full h-full object-contain" />
+				<img src={HeroImg || "/placeholder.svg"} alt="3D Artwork" className="w-full h-full object-contain" />
 			</div>
 
 			{/* Content */}
